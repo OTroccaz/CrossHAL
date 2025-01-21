720db7cdf980f76d41cd236e42142687ce274a3e
Olivier Troccaz <olivier.troccaz@univ-rennes1.fr>
2025-01-21 15:37:06 +0100

    Update CrossHAL.php

--- CrossHAL.php
+++ CrossHAL.php
@@ -175,14 +175,13 @@ include("./Glob_normalize.php");
 	<!-- third party js ends -->
 	<script src="./assets/js/pages/hal-ur1.chartjs.js"></script>
 	
+	<?php
+	//Stats Google
+	if (strpos($_SERVER['HTTP_HOST'], '127.0.0.1') === false) {
+		include("./Glob_gstats.php");
+	}
+	?>
 </head>
-
-<?php
-//Stats Google
-if (strpos($_SERVER['HTTP_HOST'], '127.0.0.1') === false) {
-	include("./Glob_gstats.php");
-}
-?>
 
 <body class="loading" data-layout="topnav" >
 
